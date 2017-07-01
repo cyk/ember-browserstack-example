8a88f89ec673afe441466b7742ac09066e9154ef
cyk <cytklassen@gmail.com>
2017-06-30 17:43:09 -0700

    I give you some routes and an acceptance test

--- app/router.js
+++ app/router.js
@@ -7,6 +7,7 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('detour');
 });
 
 export default Router;
---
+++ tests/acceptance/welcome-test.js
@@ -0,0 +1,16 @@
+import { test } from 'qunit';
+import moduleForAcceptance from 'ember-browserstack-example/tests/helpers/module-for-acceptance';
+
+moduleForAcceptance('Acceptance | welcome');
+
+test('visiting /', function(assert) {
+  visit('/');
+  andThen(() => assert.equal(find('#title').text(), 'Congratulations, you made it!'));
+});
+
+
+test('taking a detour', function(assert) {
+  visit('/detour');
+  click('.continue');
+  andThen(() => assert.equal(currentURL(), '/'));
+});
